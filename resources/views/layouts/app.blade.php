ae86900586376fcd613aa77aaa9532f0ba02759b
Unknown <irvan.2208@gmail.com>
2018-04-25 14:23:16 +0700

    Update: layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,32 +7,32 @@
 <!--===============================================================================================-->
 	<link rel="icon" type="image/png" href="images/icons/favicon.png"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+	{{ Html::style('vendor/bootstrap/css/bootstrap.min.css') }}
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+	{{ Html::style('fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/themify/themify-icons.css">
+	{{ Html::style('fonts/themify/themify-icons.css') }}
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+	{{ Html::style('fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="fonts/elegant-font/html-css/style.css">
+	{{ Html::style('fonts/elegant-font/html-css/style.css') }}
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+	{{ Html::style('vendor/animate/animate.css') }}
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+	{{ Html::style('vendor/css-hamburgers/hamburgers.min.css') }}
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+	{{ Html::style('vendor/animsition/css/animsition.min.css') }}
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+	{{ Html::style('vendor/select2/select2.min.css') }}
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+	{{ Html::style('vendor/daterangepicker/daterangepicker.css') }}
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/slick/slick.css">
+	{{ Html::style('vendor/slick/slick.css') }}
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="vendor/lightbox2/css/lightbox.min.css">
+	{{ Html::style('vendor/lightbox2/css/lightbox.min.css') }}
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="css/util.css">
-	<link rel="stylesheet" type="text/css" href="css/main.css">
+	{{ Html::style('css/util.css') }}
+	{{ Html::style('css/main.css') }}
 <!--===============================================================================================-->
 </head>
 <body class="animsition">
@@ -1918,14 +1918,14 @@
 	</div>
 
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	{{ Html::script('vendor/jquery/jquery-3.2.1.min.js') }}
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/animsition/js/animsition.min.js"></script>
+	{{ Html::script('vendor/animsition/js/animsition.min.js') }}
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/bootstrap/js/popper.js"></script>
-	<script type="text/javascript" src="vendor/bootstrap/js/bootstrap.min.js"></script>
+	{{ Html::script('vendor/bootstrap/js/popper.js') }}
+		{{ Html::script('vendor/bootstrap/js/bootstrap.min.js') }}
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/select2/select2.min.js"></script>
+	{{ Html::script('vendor/select2/select2.min.js') }}
 	<script type="text/javascript">
 		$(".selection-1").select2({
 			minimumResultsForSearch: 20,
@@ -1933,14 +1933,14 @@
 		});
 	</script>
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/slick/slick.min.js"></script>
-	<script type="text/javascript" src="js/slick-custom.js"></script>
+	{{ Html::script('vendor/slick/slick.min.js') }}
+	{{ Html::script('js/slick-custom.js') }}
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/countdowntime/countdowntime.js"></script>
+	{{ Html::script('vendor/countdowntime/countdowntime.js') }}
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/lightbox2/js/lightbox.min.js"></script>
+	{{ Html::script('vendor/lightbox2/js/lightbox.min.js') }}
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/sweetalert/sweetalert.min.js"></script>
+	{{ Html::script('vendor/sweetalert/sweetalert.min.js') }}
 	<script type="text/javascript">
 		$('.block2-btn-addcart').each(function(){
 			var nameProduct = $(this).parent().parent().parent().find('.block2-name').html();
@@ -1958,12 +1958,11 @@
 	</script>
 
 <!--===============================================================================================-->
-	<script type="text/javascript" src="vendor/parallax100/parallax100.js"></script>
+	{{ Html::script('vendor/parallax100/parallax100.js') }}
 	<script type="text/javascript">
         $('.parallax100').parallax100();
 	</script>
 <!--===============================================================================================-->
-	<script src="js/main.js"></script>
-
+	{{ Html::script('js/main.js') }}
 </body>
 </html>
